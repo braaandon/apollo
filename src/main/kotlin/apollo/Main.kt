96b182249af6650f428b6cd4a60845e46569b66b
moveq5 <86573128+moveq5@users.noreply.github.com>
2021-07-17 12:56:45 +0100

    remove accidental pushed code

--- src/main/kotlin/apollo/Main.kt
+++ src/main/kotlin/apollo/Main.kt
@@ -1,13 +1,3 @@
 package apollo
 
-import com.sun.jna.*
-import apollo.util.Process;
-
-fun main() {
-    val p = Process()
-
-    val my_int = Memory(Int.SIZE_BYTES.toLong())
-    my_int.setInt(0, 4445)
-    
-    p.write(0x2414565, my_int)
-}
+fun main() {}
