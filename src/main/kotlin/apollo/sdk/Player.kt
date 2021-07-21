ada6bce12fc5dfc9cc6c740be9ac6cfe750f2240
moveq5 <86573128+moveq5@users.noreply.github.com>
2021-07-21 17:22:18 +0100

    make speed work on multiplayer

--- src/main/kotlin/apollo/sdk/Player.kt
+++ src/main/kotlin/apollo/sdk/Player.kt
@@ -10,13 +10,19 @@ object Player {
     }
 
     fun setSpeed(value: Float) {
-        val base = Game.resolveMultiLvl(
+        val sp = Game.resolveMultiLvl(
             Game.mod_base + 0x0976F950,
             arrayListOf(0x2E0, 0x218, 0x100, 0x8, 0x8, 0x290, 0x108)
         )
 
+        val mp = Game.resolveMultiLvl(
+            Game.mod_base + 0x09872110,
+            arrayListOf(0x18, 0x88, 0x80, 0x78, 0x40, 0x248, 0x268)
+        )
+
         val mem = Memory(Float.SIZE_BYTES.toLong())
         mem.setFloat(0, value)
-        Game.write(base + 0x84, mem)
+        Game.write(sp + 0x84, mem)
+        Game.write(mp + 0x84, mem)
     }
 }
